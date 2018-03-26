ed7cb6b672a9fead19feec6627222870f9d03f0d
Spencer Wallace <dude.wallace@gmail.com>
2018-03-26 15:07:55 -0600

    Updated the layouts

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends ('layout/layout')
+
+
+@section('content')
+<div class="container">
+    <div class="jumbotron">
+        <h1>Welcome to Redflag Deal Alerts</h1>
+    </div>
+</div>
+@endsection
