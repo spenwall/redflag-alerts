9b66c09e78e3b21670eea19b65c41f49a183db19
spenwall <dude.wallace@gmail.com>
2018-04-25 21:41:35 -0600

    Updated scraper to store information in the db

--- tests/Feature/ScraperTest.php
+++ tests/Feature/ScraperTest.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Scraper;
 use App\User;
 use App\Alert;
+use App\Posts;
 use SebastianBergmann\Comparator\Factory;
 
 class ScraperTest extends TestCase
@@ -25,37 +26,7 @@ class ScraperTest extends TestCase
     {
         $scraper = new Scraper();
         $scraper->storeNewPosts();
-        //scape a page and store any new posts
-        //Store any new posts
-    }
-
-    /** @test */
-    public function scraper_can_search_for_the_off_keyword_for_the_main_page()
-    {
-        $scraper = new Scraper();
-
-        $results = $scraper->searchPageForKeywords('off');
-
-        $this->assertNotEmpty($results);
-    }
-
-    /** @test */
-    public function scraper_can_search_all_user_alerts_for_main_page()
-    {
-        $user = factory(User::class)->create();
-        $alerts = factory(Alert::class)->create([
-            'user_id' => $user->id,
-            'keywords' => 'off',
-        ]);
-        $alerts = factory(Alert::class)->create([
-            'user_id' => $user->id,
-            'keywords' => 'free',
-        ]);
-
-        $scraper = new Scraper();
         
-        $results = $scraper->searchUserAlerts($user);
-
-        $this->assertNotEmpty($results);
+        $this->assertNotNull(Posts::all());
     }
 }
