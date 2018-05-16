967da3337d8f71d6d84b7573be297dfb2be4b0ea
Spencer Wallace <dude.wallace@gmail.com>
2018-05-16 13:59:41 -0600

    Alert create component updated with dynamic error checking

--- resources/assets/js/components/CreateAlert.vue
+++ resources/assets/js/components/CreateAlert.vue
@@ -1,28 +1,100 @@
 <template>
-    <form method="POST" action="/alerts">
-        <input type="hidden" name="_token" :value="csrf">
+    <form method="POST" action="/alerts" @submit.prevent="onSubmit" @keydown="errors.clear($event.target.name)">
         <div class="form-group">
                 <label for="name">Alert Name</label>
                 <input type="text" class="form-control" id="name" 
-                        name="name" placeholder="Alert Name" required>
+                        name="name" placeholder="Alert Name" v-model="name"
+                        >
+                <span class="help text-danger" v-if="errors.has('name')" v-text="errors.get('name')"></span>
+                
         </div>
 
         <div class="form-group">
                 <label for="keywords">Search Keywords</label>
                 <input type="text" class="form-control" id="keywords" 
-                        name="keywords" placeholder="Search Keywords" required>
+                        name="keywords" placeholder="Search Keywords" v-model="keywords">
+                <span class="help text-danger" v-if="errors.has('keywords')" v-text="errors.get('keywords')"></span>
         </div>
         
-        <button type="submit" class="btn btn-primary float-right">Submit</button>
+        <button type="submit" class="btn btn-primary float-right" :disabled="errors.any()">Submit</button>
     </form>
 </template>
 
+
 <script>
+
 export default {
     data() {
         return {
-            csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+            name: '',
+            keywords: '',
+            errors: new Errors(),
         }
+    },
+
+    methods: {
+
+        onSubmit() {
+            
+            axios.post('/alerts', this.$data)
+                .then(response => {
+                    this.onSuccess(response)
+                })
+                .catch(error => {
+                    this.errors.record(error.response.data.errors)
+                })
+        },
+
+        onSuccess(response) {
+
+            this.name = ''
+            this.keywords = ''
+
+        }
+
+    }
+}
+
+class Errors {
+
+    constructor() {
+
+        this.errors = {}
+
+    } 
+
+    get(field) {
+
+        if (this.errors[field]) {
+
+            return this.errors[field][0]
+
+        }
+
+    }
+
+    record(errors) {
+
+        this.errors = errors
+
+    }
+
+    clear(field) {
+
+        delete this.errors[field]
+
+    }
+
+    has(field) {
+
+        return this.errors.hasOwnProperty(field)
+
+    }
+    
+    any() {
+
+        return Object.keys(this.errors).length > 0
+
     }
 }
 </script>
--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -6,20 +6,6 @@
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Alerts
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="{{ route('alerts') }}">Alerts</a>
-          <a class="dropdown-item" href="{{ route('create-alert') }}">Add Alert</a>
-        </div>
-      </li>
-    </ul>
     <ul class="navbar-nav ml-auto">
       <!-- Authentication Links -->
       @guest
