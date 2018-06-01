4006a9dcbf1cdde0d552e1fa56f52fed82ba72c0
spenwall <dude.wallace@gmail.com>
2018-06-01 00:17:49 -0600

    Add cypress test for new account

---
+++ cypress/integration/new_account_test.js
@@ -0,0 +1,21 @@
+describe('My First Test', function () {
+    it('Basic Test', function () {
+        //Arrange
+        cy.visit('/register')
+
+        //act
+        cy.get('#name').type('Spencer Test')
+        cy.get('#email').type('test10@gmail.com')
+        cy.get('#phone').type(4035557373)
+        cy.get('#password').type('testpassword')
+        cy.get('#password-confirm').type('testpassword')
+        cy.get('button[type=submit]').click() 
+
+        cy.get('#add-alert').click() 
+        cy.get('#name').type('test name')
+        cy.get('#keywords').type('off')
+        cy.get('button[type=submit]').click()
+        cy.wait(500)
+        cy.get('.alert-item').should('contain', 'off')
+    })
+})
