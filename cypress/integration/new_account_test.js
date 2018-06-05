e1273f245e4f32fdad4a9b22cafb778dd5ca03b3
spenwall <dude.wallace@gmail.com>
2018-06-05 07:50:15 -0600

    New Cypress test

--- cypress/integration/new_account_test.js
+++ cypress/integration/new_account_test.js
@@ -1,21 +1,23 @@
 describe('My First Test', function () {
-    it('Basic Test', function () {
+    it('Register Test', function () {
         //Arrange
-        cy.visit('/register')
+        cy.visit('/register');
 
         //act
-        cy.get('#name').type('Spencer Test')
-        cy.get('#email').type('test10@gmail.com')
-        cy.get('#phone').type(4035557373)
-        cy.get('#password').type('testpassword')
-        cy.get('#password-confirm').type('testpassword')
-        cy.get('button[type=submit]').click() 
+        let randomAccount = Math.floor((Math.random() * 100) + 1);
+        cy.get('#name').type('Some Name');
+        cy.get('#email').type('test'+ randomAccount + '@gmail.com');
+        cy.get('#phone').type(4035557373);
+        cy.get('#password').type('testpassword');
+        cy.get('#password-confirm').type('testpassword');
+        cy.get('button[type=submit]').click();
 
-        cy.get('#add-alert').click() 
-        cy.get('#name').type('test name')
-        cy.get('#keywords').type('off')
-        cy.get('button[type=submit]').click()
-        cy.wait(500)
-        cy.get('.alert-item').should('contain', 'off')
-    })
-})
+        cy.get('#add-alert').click(); 
+        cy.get('#name').type('test name');
+        cy.get('#keywords').type('off');
+        cy.get('button[type=submit]').click();
+        cy.wait(500);
+        cy.get('.alert-item').should('contain', 'off');
+
+    });
+});
