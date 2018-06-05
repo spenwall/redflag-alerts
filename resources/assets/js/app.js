9a2ba9153f4cdfb64c4ed9ea70e74878d08d2d21
spenwall <dude.wallace@gmail.com>
2018-06-05 07:50:49 -0600

    Add semi colons

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,19 +1,19 @@
-require('bulma')
-require('animate.css')
+require('bulma');
+require('animate.css');
 
-window.axios = require('axios')
-window.Vue = require('vue')
+window.axios = require('axios');
+window.Vue = require('vue');
 
-import Alerts from './components/Alerts.vue'
-import Modal from './components/Modal.vue'
-import CreateAlert from './components/CreateAlert.vue'
-import AddAlert from './components/AddAlert.vue'
+import Alerts from './components/Alerts.vue';
+import Modal from './components/Modal.vue';
+import CreateAlert from './components/CreateAlert.vue';
+import AddAlert from './components/AddAlert.vue';
 
 
-Vue.component('alerts-view', Alerts)
-Vue.component('modal', Modal)
-Vue.component('create-alert', CreateAlert)
-Vue.component('add-alert', AddAlert)
+Vue.component('alerts-view', Alerts);
+Vue.component('modal', Modal);
+Vue.component('create-alert', CreateAlert);
+Vue.component('add-alert', AddAlert);
 
 
 const app = new Vue({
