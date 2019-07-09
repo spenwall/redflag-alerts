50f61db4c6a9abf31c18915a102372a1b371d950
Spencer <swallace@weddingstar.com>
2019-07-09 11:52:21 -0600

    Move testing database to memory

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'sqlite_testing' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('testing.sqlite')),
+            'database' => ':memory',
             'prefix' => '',
         ],
 
