3c1e790dea4c3cc0bacb3d2c43ea170bb06c4f28
Spencer <swallace@weddingstar.com>
2019-07-12 09:53:23 -0600

    Fixed bad class namespace

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if(env('FORCE_HTTPS')) {
-            URL::forceScheme('https');
+            \URL::forceScheme('https');
         }
     }
 
